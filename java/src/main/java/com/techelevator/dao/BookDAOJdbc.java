ffd3251e8ac8ce2137be35e6bc4cae92496bb6e7
Conrad Estes <Conrad.Estes@gmail.com>
2022-05-31 23:31:55 -0400

    added series to book model and jdbc

--- java/src/main/java/com/techelevator/dao/BookDAOJdbc.java
+++ java/src/main/java/com/techelevator/dao/BookDAOJdbc.java
@@ -26,12 +26,13 @@ public class BookDAOJdbc implements BookDAO {
      * String description
      * Date initialPublishDate
      * String imgUrl
+     * String series
      * @return List books
      */
     @Override
     public List<Book> listBooks() {
         List <Book> books = new ArrayList<>();
-        String sql = "SELECT book_id, title, isbn, description, published_date, cover_art FROM book";
+        String sql = "SELECT book_id, title, isbn, description, published_date, cover_art, series_id FROM book;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         while(results.next()) {
             Book book = mapRowToBook(results);
@@ -55,7 +56,7 @@ public class BookDAOJdbc implements BookDAO {
         List <String> authors = new ArrayList<>();
         String sql = "SELECT author_name FROM author "
                 +"JOIN book_author ON author.author_id = book_author.author_id "
-                +"WHERE book_id = ( SELECT book_id FROM book WHERE book_id = ? )";
+                +"WHERE book_id = ( SELECT book_id FROM book WHERE book_id = ? );";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, bookID);
         while(results.next()) {
             String author = results.getString("author_name");
@@ -68,13 +69,26 @@ public class BookDAOJdbc implements BookDAO {
         List<String> genres = new ArrayList<>();
         String sql = "SELECT genre_name FROM genre "
                 + "JOIN book_genre ON genre.genre_id = book_genre.genre_id "
-                + "WHERE book_id = (SELECT book_id FROM book WHERE book_id = ?)";
+                + "WHERE book_id = (SELECT book_id FROM book WHERE book_id = ?);";
        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, bookID);
         while(results.next()) {
             String genre = results.getString("genre_name");
             genres.add(genre);
         }
         return genres;
+    }
+
+    private String getBookSeriesBySeriesId(int seriesID){
+        String series = "";
+        String sql = "SELECT series_name FROM series WHERE series_id = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, seriesID);
+
+        if(results.next()){
+            series = results.getString("series_name");
+        }
+
+        return series;
     }
 
     private Book mapRowToBook(SqlRowSet rowSet) {
@@ -88,7 +102,7 @@ public class BookDAOJdbc implements BookDAO {
         book.setInitialPublishDate(rowSet.getDate("published_date").toLocalDate());
         book.setAuthors( listOfAuthorsByBookID( rowSet.getInt("book_id") ) );
         book.setGenres( listOfGenresByBookID( rowSet.getInt("book_id") ) );
-
+        book.setSeries(getBookSeriesBySeriesId(rowSet.getInt("series_id")));
         return book;
     }
 }
